e34b24bcb68c49a9c4cc33db8bf6c2f3fcac76ac
Aditya Sinha <62201019+aditya7sinha@users.noreply.github.com>
2021-09-27 15:15:41 +0530

    Completed module

--- src/app.js
+++ src/app.js
@@ -5,14 +5,30 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.render('index', { title: 'Account Summary'}));
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+// app.get('/savings', (res,res) => res.render('account', { account:accounts.savings}));
+// app.get('/checking',(req,res)=> res.render('checking',{account:accounts.checking}));
+// app.get('/credit', (req,res) => res.render('credit',{account: accounts.credit}));
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings })
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking })
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit })
+});
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
+
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
 
 
-// //read account data
-// const accountData = fs.readFileSync('src/json/accounts.json').toString('utf-8');
-// const accounts = JSON.parse(accountData);
+//read account data
+const accountData = fs.readFileSync('src/json/accounts.json').toString('utf-8');
+const accounts = JSON.parse(accountData);
 
-// //read user data
-// const userData = fs.readFileSync('src/json/users.json').toString('utf-8');
-// const users = JSON.parse(userData);
+//read user data
+const userData = fs.readFileSync('src/json/users.json').toString('utf-8');
+const users = JSON.parse(userData);
