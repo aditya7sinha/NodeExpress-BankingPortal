e23eab76f69ed692b629d0886121ab1e8525a0f6
Aditya Sinha <62201019+aditya7sinha@users.noreply.github.com>
2021-09-28 18:35:46 +0530

    completed module

--- src/app.js
+++ src/app.js
@@ -6,16 +6,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+
+
 //read account data
-const accountData = fs.readFileSync('src/json/accounts.json','utf8');
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
 const accounts = JSON.parse(accountData);
 
 //read user data
-const userData = fs.readFileSync('src/json/users.json','utf8');
+const userData = fs.readFileSync('src/json/users.json', 'utf8');
 const users = JSON.parse(userData);
 
-
-
+//routing
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 // app.get('/savings', (res,res) => res.render('account', { account:accounts.savings}));
 // app.get('/checking',(req,res)=> res.render('checking',{account:accounts.checking}));
@@ -32,6 +34,26 @@ app.get('/credit', (req, res) => {
     res.render('account', { title: 'Credit Account', account: accounts.credit })
 });
 app.get('/profile', (req, res) => res.render('profile', { title: 'User Profile', user: users[0] }));
+
+app.get('/transfer', (req, res) => res.render('transfer'));
+
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance = accounts.credit.balance - req.body.amount;
+    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', { message: 'Payment Completed' });
+})
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
 
