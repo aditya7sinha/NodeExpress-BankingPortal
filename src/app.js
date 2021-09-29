c4b74e0a5a69596eb0379f1df95d9da20e3c18c8
Aditya Sinha <62201019+aditya7sinha@users.noreply.github.com>
2021-09-29 14:11:39 +0530

    Completed module

--- src/app.js
+++ src/app.js
@@ -2,16 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 // const { accounts, users, writeJSON } = require('./data');
-const {accounts, users, writeJSON} = require('./data');
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
+const { accounts, users, writeJSON } = require('./data.js');
 const express = require('express');
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 //read account data
 // const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
 // const accounts = JSON.parse(accountData);
@@ -25,40 +29,9 @@ app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accou
 // app.get('/savings', (res,res) => res.render('account', { account:accounts.savings}));
 // app.get('/checking',(req,res)=> res.render('checking',{account:accounts.checking}));
 // app.get('/credit', (req,res) => res.render('credit',{account: accounts.credit}));
-app.get('/savings', (req, res) => {
-    res.render('account', { title: 'Savings Account', account: accounts.savings })
-});
 
-app.get('/checking', (req, res) => {
-    res.render('account', { title: 'Checking Account', account: accounts.checking })
-});
-
-app.get('/credit', (req, res) => {
-    res.render('account', { title: 'Credit Account', account: accounts.credit })
-});
 app.get('/profile', (req, res) => res.render('profile', { title: 'User Profile', user: users[0] }));
 
-app.get('/transfer', (req, res) => res.render('transfer'));
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    // const accountsJSON = JSON.stringify(accounts, null, 4);
-    // fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
-    writeJSON(accounts);
-    res.render('transfer', { message: 'Transfer Completed' });
-});
-
-app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance = accounts.credit.balance - req.body.amount;
-    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount, 10);
-    // const accountsJSON = JSON.stringify(accounts, null, 4);
-    // fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
-    writeJSON(accounts);
-    res.render('payment', { message: 'Payment Completed' });
-})
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
 
